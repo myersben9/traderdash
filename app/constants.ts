8c3e613636534936e1c3712654e353b66423beb0
myersben9 <120344142+myersben9@users.noreply.github.com>
2025-04-01 15:56:42 -0700

    debug

--- app/constants.ts
+++ app/constants.ts
@@ -6,8 +6,11 @@ export const getHost = () => {
     } else if (process.env.NEXT_PUBLIC_ENV === 'production') {
         return `https://${process.env.NEXT_PUBLIC_PROD_BACKEND_URL}`;
 
+    } else if (process.env.NEXT_PUBLIC_ENV === 'preview') {
+        return `https://${process.env.NEXT_PUBLIC_STAGING_BACKEND_URL}`;
+
     } else {
-        console.log('Unknown environment detected');
-        throw new Error('Unknown environment detected');
+        console.log(process.env.NEXT_PUBLIC_NODE_ENV);
+        throw new Error('Invalid environment variable. Please set NEXT_PUBLIC_ENV to either development, production, or staging.');
     }
 }
