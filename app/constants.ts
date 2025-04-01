c17a3586b2928e26056ec86655c199fb20677309
myersben9 <120344142+myersben9@users.noreply.github.com>
2025-04-01 15:58:04 -0700

    fix

--- app/constants.ts
+++ app/constants.ts
@@ -3,10 +3,10 @@ export const getHost = () => {
     if (process.env.NEXT_PUBLIC_NODE_ENV === 'development') {
         return `http://${process.env.NEXT_PUBLIC_DEV_BACKEND_URL}`;
 
-    } else if (process.env.NEXT_PUBLIC_ENV === 'production') {
+    } else if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
         return `https://${process.env.NEXT_PUBLIC_PROD_BACKEND_URL}`;
 
-    } else if (process.env.NEXT_PUBLIC_ENV === 'preview') {
+    } else if (process.env.NEXT_PUBLIC_NODE_ENV === 'preview') {
         return `https://${process.env.NEXT_PUBLIC_STAGING_BACKEND_URL}`;
 
     } else {
